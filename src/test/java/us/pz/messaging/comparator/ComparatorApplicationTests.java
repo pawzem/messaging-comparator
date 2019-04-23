0c670c71771fdec5d313de6c2495c6de4caf0073
Paweł Żemła <pawzem@gmail.com>
2019-04-23 12:11:58 +0200

    initial commit

---
+++ src/test/java/us/pz/messaging/comparator/ComparatorApplicationTests.java
@@ -0,0 +1,16 @@
+package us.pz.messaging.comparator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComparatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
