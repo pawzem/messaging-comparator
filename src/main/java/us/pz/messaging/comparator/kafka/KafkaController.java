a3e260b949eeca368e9727e95b10df265b9cfacc
Paweł Żemła <pawzem@gmail.com>
2019-04-24 14:11:46 +0200

    kafka jmter basic plan

--- src/main/java/us/pz/messaging/comparator/kafka/KafkaController.java
+++ src/main/java/us/pz/messaging/comparator/kafka/KafkaController.java
@@ -1,46 +1,62 @@
 package us.pz.messaging.comparator.kafka;
 
 import lombok.AllArgsConstructor;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @AllArgsConstructor
-class KafkaController {
+public class KafkaController {
     private final KafkaFacade kafkaFacade;
 
-    @PostMapping("kafka/1kb")
-    public ResponseEntity send1KbMessage() {
+
+    @GetMapping("/kafka")
+    Resource getResources() {
+        return new Resource<>("Kafka links",
+                linkTo(methodOn(KafkaController.class).send1KbMessage()).withSelfRel(),
+                linkTo(methodOn(KafkaController.class).send1MbMessage()).withSelfRel(),
+                linkTo(methodOn(KafkaController.class).send10MbMessage()).withSelfRel(),
+                linkTo(methodOn(KafkaController.class).send100MbMessage()).withSelfRel(),
+                linkTo(methodOn(KafkaController.class).send1GbMessage()).withSelfRel()
+        );
+    }
+
+    @PostMapping("/kafka/1kb")
+    ResponseEntity send1KbMessage() {
         kafkaFacade.send1KbMessage();
         return new ResponseEntity(OK);
     }
 
-    @PostMapping("kafka/1mb")
-    public ResponseEntity send1MbMessage() {
+    @PostMapping("/kafka/1mb")
+    ResponseEntity send1MbMessage() {
         kafkaFacade.send1MbMessage();
         return new ResponseEntity(OK);
     }
 
 
-    @PostMapping("kafka/10mb")
-    public ResponseEntity send10MbMessage() {
+    @PostMapping("/kafka/10mb")
+    ResponseEntity send10MbMessage() {
         kafkaFacade.send10MbMessage();
         return new ResponseEntity(OK);
     }
 
 
-    @PostMapping("kafka/100mb")
-    public ResponseEntity send100MbMessage() {
+    @PostMapping("/kafka/100mb")
+    ResponseEntity send100MbMessage() {
         kafkaFacade.send100MbMessage();
         return new ResponseEntity(OK);
     }
 
 
-    @PostMapping("kafka/1gb")
-    public ResponseEntity send1GbMessage() {
+    @PostMapping("/kafka/1gb")
+    ResponseEntity send1GbMessage() {
         kafkaFacade.send1GbMessage();
         return new ResponseEntity(OK);
     }
