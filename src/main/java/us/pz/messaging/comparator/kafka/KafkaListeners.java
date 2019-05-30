bbca431cf6b089e2127d0214e4cc217997fc6d06
Paweł Żemła <pawzem@gmail.com>
2019-05-30 13:08:52 +0200

    parametrized Kafka topics

--- src/main/java/us/pz/messaging/comparator/kafka/KafkaListeners.java
+++ src/main/java/us/pz/messaging/comparator/kafka/KafkaListeners.java
@@ -10,37 +10,37 @@ import org.springframework.stereotype.Service;
 @Slf4j
 class KafkaListeners {
 
-    @KafkaListener(id = "1kb", topics = "single-partition-1kb")
+    @KafkaListener(id = "1kb", topics = "${topics.topic1kb}")
     public void listen1kb(String message) {
         log.info("received 1kb message");
     }
 
-    @KafkaListener(id = "100kb", topics = "single-partition-100kb")
+    @KafkaListener(id = "100kb", topics = "${topics.topic100kb}")
     public void listen100kb(String message) {
         log.info("received 100kb message");
     }
 
-    @KafkaListener(id = "1Mb", topics = "single-partition-1Mb")
+    @KafkaListener(id = "1Mb", topics = "${topics.topic1Mb}")
     public void listen1Mb(String message) {
         log.info("received 1Mb message");
     }
 
-    @KafkaListener(id = "10Mb", topics = "single-partition-10Mb")
+    @KafkaListener(id = "10Mb", topics = "${topics.topic10Mb}")
     public void listen10Mb(String message) {
         log.info("received 10Mb message");
     }
 
-    @KafkaListener(id = "100Mb", topics = "single-partition-100Mb")
+    @KafkaListener(id = "100Mb", topics = "${topics.topic100Mb}")
     public void listen100Mb(String message) {
         log.info("received 100Mb message");
     }
 
-    @KafkaListener(id = "1Gb", topics = "single-partition-1Gb")
+    @KafkaListener(id = "1Gb", topics = "${topics.topic1Gb}")
     public void listen1Gb(String message) {
         log.info("received 1Gb message");
     }
 
-    @KafkaListener(id = "growing", topics = "single-partition-growing")
+    @KafkaListener(id = "growing", topics = "${topics.topicGrowing}")
     public void listenGrowing(String message) {
         log.info("received growing message");
     }
